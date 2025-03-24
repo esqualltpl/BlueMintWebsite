bf64652a64ff921f9de8a2c2eaf67813063acd4b
Muhammad Baig <m.alybaig@gmail.com>
2025-03-24 17:50:16 +0500

    font issue fixed

--- src/authentication/signup/signup.component.ts
+++ src/authentication/signup/signup.component.ts
@@ -1,11 +1,15 @@
+import { NgIf } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms'; // ✅ Import FormsModule for ngModel
 
 @Component({
   selector: 'app-signup',
-  imports: [],
+  imports: [NgIf, FormsModule], // ✅ Import FormsModule here
+
   templateUrl: './signup.component.html',
   styleUrl: './signup.component.css'
 })
 export class SignupComponent {
+  selectedType: string = 'Adviser'; // Default selection
 
 }
